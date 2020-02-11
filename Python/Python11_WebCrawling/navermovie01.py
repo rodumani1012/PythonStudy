6c44a6d1562f83eee34b1c96083344547147d9cb
rodumani1012 <48143127+rodumani1012@users.noreply.github.com>
2020-02-11 13:32:39 +0900

    웹크롤링

--- Python/Python11_WebCrawling/navermovie01.py
+++ Python/Python11_WebCrawling/navermovie01.py
@@ -18,7 +18,26 @@ resp = urllib.request.urlopen('https://movie.naver.com/movie/running/current.nhn
 
 # resp 파싱하기.
 soup = BeautifulSoup(resp, 'html.parser')
-print(soup)
+# print(soup)
 
 # dl 태그에서 클래스명이 lst_dsc인 것 찾아오기
 movieList = soup.findAll('dl', {'class','lst_dsc'})
+# print(movieList)
+
+# 이름과 별점 출력하기
+for movieOne in movieList:
+    link = movieOne.find('a') # 리스트 안에 a태그 검색
+    num = movieOne.select('.num')[0] # 클래스명 검색
+    # print('%s %s' % (link.text, num.text))
+
+# 이미지 소스 출력하기
+movieImg = soup.findAll('div', {'class', 'thumb'})
+
+for imgOne in movieImg:
+    imgSrc = imgOne.find('img')
+    # print(imgSrc.get('src'))
+
+try:
+    print(str(link.text).split(' ')[1])
+except IndexError:
+    print(str(link.text).split(' ')[0])
